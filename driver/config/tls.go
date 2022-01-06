2e413656f2834fbd3905f2e5f04285645e49c0f3
Jorgen Heinsoo <jorgen.heinsoo@nortal.com>
2022-01-06 10:04:40 +0200

    PR-2875 -  feat: backchannel logout request client tls configuration

--- driver/config/tls.go
+++ driver/config/tls.go
@@ -2,6 +2,10 @@ package config
 
 import (
 	"crypto/tls"
+	"strings"
+
+	"github.com/hashicorp/go-secure-stdlib/tlsutil"
+	"github.com/pkg/errors"
 
 	"github.com/ory/x/tlsx"
 )
@@ -19,6 +23,10 @@ const (
 	KeyTLSKeyString            = "serve." + KeySuffixTLSKeyString
 	KeyTLSCertPath             = "serve." + KeySuffixTLSCertPath
 	KeyTLSKeyPath              = "serve." + KeySuffixTLSKeyPath
+
+	KeyClientTLSCipherSuites = "client.tls.cipher_suites"
+	KeyClientTLSMinVer       = "client.tls.min_version"
+	KeyClientTLSMaxVer       = "client.tls.max_version"
 )
 
 type TLSConfig interface {
@@ -45,6 +53,39 @@ func (p *Provider) TLS(iface ServeInterface) TLSConfig {
 		certPath:   p.p.StringF(iface.Key(KeySuffixTLSCertPath), p.p.String(KeyTLSCertPath)),
 		keyPath:    p.p.StringF(iface.Key(KeySuffixTLSKeyPath), p.p.String(KeyTLSKeyPath)),
 	}
+}
+
+func (p *Provider) TLSClientConfig() (*tls.Config, error) {
+	tlsClientConfig := new(tls.Config)
+
+	if p.p.Exists(KeyClientTLSCipherSuites) {
+		keyCipherSuites := p.p.Strings(KeyClientTLSCipherSuites)
+		cipherSuites, err := tlsutil.ParseCiphers(strings.Join(keyCipherSuites[:], ","))
+		if err != nil {
+			return nil, errors.WithMessage(err, "Unable to setup client TLS configuration")
+		}
+		tlsClientConfig.CipherSuites = cipherSuites
+	}
+
+	if p.p.Exists(KeyClientTLSMinVer) {
+		keyMinVer := p.p.String(KeyClientTLSMinVer)
+		if tlsMinVer, found := tlsutil.TLSLookup[keyMinVer]; !found {
+			return nil, errors.Errorf("Unable to setup client TLS configuration. Invalid minimum TLS version: %s", keyMinVer)
+		} else {
+			tlsClientConfig.MinVersion = tlsMinVer
+		}
+	}
+
+	if p.p.Exists(KeyClientTLSMaxVer) {
+		keyMaxVer := p.p.String(KeyClientTLSMaxVer)
+		if tlsMaxVer, found := tlsutil.TLSLookup[keyMaxVer]; !found {
+			return nil, errors.Errorf("Unable to setup client TLS configuration. Invalid maximum TLS version: %s", keyMaxVer)
+		} else {
+			tlsClientConfig.MaxVersion = tlsMaxVer
+		}
+	}
+
+	return tlsClientConfig, nil
 }
 
 type tlsConfig struct {

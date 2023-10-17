88370c759e379f85cd961dfbed1ef3f2ec0f8b30
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2023-10-17 14:31:38 +0300

    AUT-1550 Fix closing HSM session in server shutdown

---
+++ cmd/server/stop_notifier.go
@@ -0,0 +1,51 @@
+package server
+
+import "sync"
+
+type void struct{}
+
+type StopNotifier struct {
+	channels []chan void
+	mutex    sync.Mutex
+	stopped  bool
+}
+
+func NewStopNotifier() *StopNotifier {
+	return &StopNotifier{
+		stopped: false,
+	}
+}
+
+func (self *StopNotifier) Notify() {
+	self.mutex.Lock()
+	defer self.mutex.Unlock()
+
+	if !self.stopped {
+		self.stopped = true
+		for _, channel := range self.channels {
+			close(channel)
+		}
+	}
+}
+
+func (self *StopNotifier) Wait() {
+	self.mutex.Lock()
+
+	if self.stopped {
+		self.mutex.Unlock()
+		return
+	}
+
+	channel := make(chan void)
+	self.channels = append(self.channels, channel)
+	self.mutex.Unlock()
+
+	<-channel
+}
+
+func (self *StopNotifier) Then(onStop func()) {
+	go func() {
+		self.Wait()
+		onStop()
+	}()
+}
